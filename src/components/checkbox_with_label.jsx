337af07ea9775d5378c156d425227e773260b352
Hamish Hall <hachall@hotmail.com>
2018-11-20 17:24:36 +0000

    testing react components with enzyme

---
+++ src/components/checkbox_with_label.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class CheckboxWithLabel extends React {
+  constructchor(props) {
+    super(props);
+    this.state = {isChecked: false};
+  }
+
+  onChange = () => {
+// TODO
+  }
+
+  render() {
+    return (
+      <label>
+        <input type="checkbox" checked={this.state.isChecked}
+          onChange={this.onChange} />
+        {this.state.isChecked ? this.props.labelOn : this.props.labelOff}
+      </label>
+    );
+  }
+}
+
+export default CheckboxWithLabel;
